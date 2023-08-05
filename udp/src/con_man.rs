fae67381e21ec8787b4fcfa12cc48a78b8bdaf8c
Dennis Kobert <dennis@kobert.dev>
2023-08-05 23:32:25 +0200

    Add broadcast method

--- udp/src/con_man.rs
+++ udp/src/con_man.rs
@@ -1,8 +1,4 @@
-use std::{
-    net::{SocketAddr, UdpSocket},
-    thread,
-    time::Duration,
-};
+use std::net::{SocketAddr, UdpSocket};
 
 pub struct ConnectionManager {
     local_socket: UdpSocket,
@@ -10,20 +6,26 @@ pub struct ConnectionManager {
 }
 
 impl ConnectionManager {
-    pub fn new(socket:UdpSocket) -> ConnectionManager {
+    pub fn new(socket: UdpSocket) -> ConnectionManager {
         ConnectionManager {
             connections: vec![],
             local_socket: socket,
         }
     }
 
-    pub fn connect(&mut self, addr:SocketAddr) {
+    pub fn connect(&mut self, addr: SocketAddr) {
         self.connections.push(addr);
         println!("Added Connection");
     }
 
-    pub fn send_to(&self, buf: &[u8], addr:&SocketAddr){
+    pub fn send_to(&self, buf: &[u8], addr: &SocketAddr) {
         println!("Sending");
         self.local_socket.send_to(buf, addr).expect("Weird :(");
+    }
+
+    pub fn broadcast(&self, buf: &[u8]) {
+        for addr in &self.connections {
+            self.send_to(buf, addr);
+        }
     }
 }
