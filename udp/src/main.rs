6dcf2e46a97e24ac4a3bb037fd609717a8aaff64
isiko404 <mail@isiko404.dev>
2023-08-05 23:25:17 +0200

    Add Connection Manager

--- udp/src/main.rs
+++ udp/src/main.rs
@@ -1,31 +1,32 @@
+mod con_man;
+mod cowconnect;
+
 use std::{
-    net::{SocketAddr, UdpSocket},
+    net::{SocketAddr, IpAddr, Ipv4Addr, UdpSocket},
     thread,
     time::Duration,
 };
-mod cowconnect;
-const ISAAK: std::net::SocketAddrV4 =
-    std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(192, 168, 12, 94), 1312);
+
+const ISAAK_PEER: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 4, 156)), 1313);
 
 fn main() -> std::io::Result<()> {
     {
         let socket = UdpSocket::bind("0.0.0.0:1312")?;
+        let mut connection_man = ConnectionManager::new(socket);
+        connection_man.connect(ISAAK_PEER);
 
         // Receives a single datagram message on the socket. If `buf` is too small to hold
         // the message, it will be cut off.
-        let mut buf = b"hello world\n";
-        for i in 0..100 {
-            socket.send_to(buf, ISAAK)?;
-            println!("Sent {} bytes", buf.len());
-            println!("Sent {}", std::str::from_utf8(buf).unwrap());
-            thread::sleep(Duration::from_millis(100));
-        }
+        let buf = b"hello world\n";
+        connection_man.send_to(buf, &ISAAK_PEER);
+        //for i in 0..100 {
+        //    socket.send_to(buf, ISAAK)?;
+        //    println!("Sent {} bytes", buf.len());
+        //    println!("Sent {}", std::str::from_utf8(buf).unwrap());
+        //    thread::sleep(Duration::from_millis(100));
+        //}
         //let (amt, src) = socket.recv_from(&mut buf[])?;
 
-        // Redeclare `buf` as slice of the received data and send reverse data back to origin.
-        //let buf = &mut buf[..amt];
-        //buf.reverse();
-        //socket.send_to(buf, &src)?;
     } // the socket is closed here
     Ok(())
 }
