106b17b2fadcca50b1ef56cc827246c9926a20a1
Dennis Kobert <dennis@kobert.dev>
2023-10-29 14:14:43 +0100

    Update audio code to be callable from rust

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ use std::sync::Arc;
 static mut CONTEXT: Option<Context> = None;
 
 #[derive(Debug)]
-pub struct Opt {
+pub struct Options {
     /// The input audio device to use
     pub input_device: String,
 
@@ -28,90 +28,26 @@ pub struct Opt {
     pub buffer_length: f32,
 }
 
-use core::ffi::c_int;
-#[no_mangle]
-pub extern "C" fn cinit(buffer_ms: f32) -> c_int {
-    match {
-        init(Opt {
-            input_device: "default".to_string(),
-            output_device: "default".to_string(),
-            buffer_length: buffer_ms,
-        })
-    } {
-        Ok(_) => 0,
-        Err(error) => {
-            eprintln!("error: {error}");
-            1
-        }
-    }
-}
-#[no_mangle]
-pub extern "C" fn cfree() {
-    let context = unsafe { CONTEXT.as_mut() }.unwrap_or_else(|| panic!("Context not initialized"));
-    let context = context as *mut Context;
-    unsafe {
-        std::mem::drop(context.read());
-    }
-}
-
-/// # Safety
-/// The caller must ensure that the buffer is large enough to hold `buffer_len` samples.
-#[no_mangle]
-pub unsafe extern "C" fn read_samples(buffer: *mut f32, buffer_len: usize) -> isize {
-    let context = unsafe { CONTEXT.as_mut() }.unwrap_or_else(|| panic!("Context not initialized"));
-
-    let mut i = 0;
-    for sample in context.c_in.pop_iter() {
-        *buffer.offset(i) = sample;
-        i += 1;
-        if i >= buffer_len as isize {
-            break;
-        }
-    }
-    i
-}
-
-/// # Safety
-/// The caller must ensure that the buffer is large enough to hold `buffer_len` samples.
-#[no_mangle]
-pub unsafe extern "C" fn write_samples(buffer: *mut f32, buffer_len: usize) -> isize {
-    let context = unsafe { CONTEXT.as_mut() }.unwrap_or_else(|| panic!("Context not initialized"));
-
-    let data = unsafe { std::slice::from_raw_parts(buffer, buffer_len) };
-    let mut i = 0;
-    for &sample in data {
-        if context.c_out.push(sample).is_ok() {
-            i += 1;
-        } else {
-            break;
-        }
-    }
-    i
-}
-
 type Consumer = ringbuf::Consumer<f32, Arc<ringbuf::SharedRb<f32, Vec<MaybeUninit<f32>>>>>;
 type Producer = ringbuf::Producer<f32, Arc<ringbuf::SharedRb<f32, Vec<MaybeUninit<f32>>>>>;
 
-struct Context {
-    _input_stream: Stream,
+pub enum Input {
+    Mic(Stream),
+    GPIO,
+    None,
+}
+
+pub struct Context {
+    _input_stream: Input,
     _output_stream: Stream,
     c_in: Consumer,
     c_out: Producer,
 }
 
-pub fn init(config: Opt) -> anyhow::Result<()> {
+pub fn create_context(config: Options) -> anyhow::Result<Context> {
     // Conditionally compile with jack if the feature is specified.
     let host = cpal::default_host();
     let opt = config;
-
-    // Find devices.
-    let input_device = if opt.input_device == "default" {
-        host.default_input_device()
-    } else {
-        host.input_devices()?
-            .find(|x| x.name().map(|y| y == opt.input_device).unwrap_or(false))
-    }
-    .expect("failed to find input device");
 
     let output_device = if opt.output_device == "default" {
         host.default_output_device()
@@ -137,6 +73,7 @@ pub fn init(config: Opt) -> anyhow::Result<()> {
     let (mut mic, c_in) = input_ring.split();
     let (c_out, mut speaker) = output_ring.split();
 
+    #[cfg(not(feature = "gpio"))]
     let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
         let mut output_fell_behind = false;
         for &sample in data {
@@ -170,7 +107,26 @@ pub fn init(config: Opt) -> anyhow::Result<()> {
         "Attempting to build both streams with f32 samples and `{:?}`.",
         config
     );
-    let input_stream = input_device.build_input_stream(&config, input_data_fn, err_fn)?;
+
+    // Find devices.
+    #[cfg(not(feature = "gpio"))]
+    let input_stream = if opt.input_device == "default" {
+        host.default_input_device()
+    } else {
+        host.input_devices()?
+            .find(|x| x.name().map(|y| y == opt.input_device).unwrap_or(false))
+    }
+    .and_then(|device| {
+        device
+            .build_input_stream(&config, input_data_fn, err_fn)
+            .ok()
+    })
+    .map(Input::Mic)
+    .unwrap_or(Input::None);
+
+    #[cfg(feature = "gpio")]
+    let input_stream = Input::GPIO;
+
     let output_stream = output_device.build_output_stream(&config, output_data_fn, err_fn)?;
     println!("Successfully built streams.");
 
@@ -179,22 +135,54 @@ pub fn init(config: Opt) -> anyhow::Result<()> {
         "Starting the input and output streams with `{}` milliseconds of latency.",
         opt.buffer_length
     );
-    input_stream.play()?;
+
+    match &input_stream {
+        Input::Mic(stream) => {
+            stream.play()?;
+        }
+        #[cfg(feature = "gpio")]
+        Input::GPIO => {
+            std::thread::spawn(move || loop {
+                std::thread::sleep(std::time::Duration::from_millis(100));
+                mic.push(0.0).unwrap();
+            });
+        }
+        _ => (),
+    };
     output_stream.play()?;
 
-    // Run for 3 seconds before closing.
-    //println!("Playing for 3 seconds... ");
-    //std::thread::sleep(std::time::Duration::from_secs(30));
-    let context = Context {
+    Ok(Context {
         _input_stream: input_stream,
         _output_stream: output_stream,
         c_in,
         c_out,
-    };
-    unsafe {
-        CONTEXT = Some(context);
+    })
+}
+
+impl Context {
+    pub fn read_samples(&mut self, buffer: &mut [f32]) -> usize {
+        let mut i = 0;
+        for sample in self.c_in.pop_iter() {
+            buffer[i] = sample;
+            i += 1;
+            if i >= buffer.len() {
+                break;
+            }
+        }
+        i
     }
-    Ok(())
+
+    pub fn write_samples(&mut self, data: &[f32]) -> isize {
+        let mut i = 0;
+        for sample in data {
+            if self.c_out.push(*sample).is_ok() {
+                i += 1;
+            } else {
+                break;
+            }
+        }
+        i
+    }
 }
 
 fn err_fn(err: cpal::StreamError) {
--- udp/src/main.rs
+++ udp/src/main.rs
@@ -10,47 +10,44 @@ use con_man::ConnectionManager;
 const ISAAK_PEER: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 4, 156)), 1313);
 
 fn main() -> std::io::Result<()> {
-    {
-        let socket = UdpSocket::bind("0.0.0.0:1312")?;
-        let mut connection_man = ConnectionManager::new(socket);
-        connection_man.connect(ISAAK_PEER);
+    let socket = UdpSocket::bind("0.0.0.0:1312")?;
+    let mut connection_man = ConnectionManager::new(socket);
+    connection_man.connect(ISAAK_PEER);
 
-        // Receives a single datagram message on the socket. If `buf` is too small to hold
-        // the message, it will be cut off.
-        let buf = b"hello world\n";
-        connection_man.send_to(buf, &ISAAK_PEER);
+    // Receives a single datagram message on the socket. If `buf` is too small to hold
+    // the message, it will be cut off.
+    let buf = b"hello world\n";
+    connection_man.send_to(buf, &ISAAK_PEER);
+    //for i in 0..100 {
+    //    socket.send_to(buf, ISAAK)?;
+    //    println!("Sent {} bytes", buf.len());
+    //    println!("Sent {}", std::str::from_utf8(buf).unwrap());
+    //    thread::sleep(Duration::from_millis(100));
+    //}
+    //let (amt, src) = socket.recv_from(&mut buf[])?;
+    let mut buf = [0; 1024];
+    let (amt, src) = connection_man.local_socket.recv_from(&mut buf)?;
 
-        // for i in 0..100 {
-        //     socket.send_to(buf, ISAAK)?;
-        //     println!("Sent {} bytes", buf.len());
-        //     println!("Sent {}", std::str::from_utf8(buf).unwrap());
-        //     thread::sleep(Duration::from_millis(100));
-        // }
-        // let (amt, src) = socket.recv_from(&mut buf[])?;
-        let mut buf = [0; 1024];
-        let (amt, src) = connection_man.local_socket.recv_from(&mut buf)?;
-
-        if let Some(datagram) = cowconnect::Datagram::from_bytes(&buf[..amt]) {
-            println!("Received {} bytes from {}", amt, src);
-            println!("Received {:?}", datagram);
-            match datagram.datagram_type {
-                DatagramType::Control(ControlType::Connect) => {
-                    println!("Received Connect");
-                    connection_man.connect(src);
-                }
-                DatagramType::Control(ControlType::Disconnect) => {
-                    println!("Received Connect");
-                    connection_man.connect(src);
-                }
-                DatagramType::Control(ControlType::Heartbeat) => {
-                    connection_man.heartbeat(src);
-                }
-                DatagramType::Audio(data) => {
-                    connection_man.update_audio_buffer(src, datagram.seq, &data);
-                }
-                _ => println!("Received something else"),
+    if let Some(datagram) = cowconnect::Datagram::from_bytes(&buf[..amt]) {
+        println!("Received {} bytes from {}", amt, src);
+        println!("Received {:?}", datagram);
+        match datagram.datagram_type {
+            DatagramType::Control(ControlType::Connect) => {
+                println!("Received Connect");
+                connection_man.connect(src);
             }
+            DatagramType::Control(ControlType::Disconnect) => {
+                println!("Received Connect");
+                connection_man.connect(src);
+            }
+            DatagramType::Control(ControlType::Heartbeat) => {
+                connection_man.heartbeat(src);
+            }
+            DatagramType::Audio(data) => {
+                connection_man.update_audio_buffer(src, datagram.seq, &data);
+            }
+            _ => println!("Received something else"),
         }
-    } // the socket is closed here
+    }
     Ok(())
 }
